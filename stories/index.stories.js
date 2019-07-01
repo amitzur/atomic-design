f5ebd08d2b1ad1b310a4b05d7e56cd4d5b21e8a3
Amit Zur <sendwithchibo@gmail.com>
2019-07-01 18:24:03 +0300

    more storybook

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,20 +1,26 @@
 import '../scss/style.scss';
+import '../scss/slideshow.scss';
 import React from 'react';
 import {addDecorator} from '@storybook/react';
 import {action} from '@storybook/addon-actions';
+import cn from 'classnames';
 
 // import {designToken, atom} from '../.storybook/utils'; // using this from another file kills HMR. Need to open an issue about this
 import {storiesOf} from '@storybook/react';
 
-const category = name => (kindName, stories) => {
-  const kind = storiesOf(`Library|${name}/${kindName}`, module);
+const category = type => name => (kindName, stories) => {
+  const kind = storiesOf(`${type}|${name}/${kindName}`, module);
   for (const story of stories) {
     kind.add(...story);
   }
 };
 
-export const designToken = category('Design Tokens');
-export const atom = category('Atoms');
+const libaryCategory = category('Library');
+const appCategory = category('Application');
+
+const designToken = libaryCategory('Design Tokens');
+const atom = libaryCategory('Atoms');
+const molecule = libaryCategory('Molecule');
 
 addDecorator(story => (
   <div
@@ -42,3 +48,78 @@ designToken('Spacing', [
 designToken('Borders', [['Lines', () => <div>lines</div>], ['Radius', () => <div>radius</div>]]);
 
 atom('Input', [['Textbox', () => <div>textbox</div>], ['Checkbox', () => <div>checkbox</div>]]);
+
+// atom('Slide show', [
+//   ['Box', () => <Box>some text here</Box>],
+//   ['Slide', () => <Slide>this is a slide</Slide>, {notes: require('./slide.md').default}],
+// ]);
+
+// function Box({children, className}) {
+//   return <div className={cn(className, 'p-4 border border-pink border-heavy')}>{children}</div>;
+// }
+
+// function Slide({children, className}) {
+//   return <div className={cn(className, 'bg-gray slide')}>{children}</div>;
+// }
+
+// molecule('Slide show', [
+//   [
+//     'Slide with a box',
+//     () => (
+//       <Slide>
+//         <Box>I'm a box</Box>
+//       </Slide>
+//     ),
+//   ],
+//   [
+//     'Slide with several boxes',
+//     () => (
+//       <Slide>
+//         <div className="p-4">
+//           <div className="d-flex flex-grow-1">
+//             <Box className="mr-3">I'm a box!!!</Box>
+//             <div className="flex-grow-1">
+//               <Box className="mb-3">box 2</Box>
+//               <Box>box 3</Box>
+//             </div>
+//           </div>
+//         </div>
+//       </Slide>
+//     ),
+//   ],
+//   [
+//     'Layout',
+//     () => (
+//       <Slide>
+//         <div className="p-4">
+//           <Layout
+//             left={() => 'Left side'}
+//             right1={() => 'Upper right'}
+//             right2={() => 'Lower right'}
+//           />
+//         </div>
+//       </Slide>
+//     ),
+//   ],
+// ]);
+
+// function Layout({left, right1, right2}) {
+//   return (
+//     <div className="d-flex flex-grow-1">
+//       <Box className="mr-3">{left()}</Box>
+//       <div className="flex-grow-1">
+//         <Box className="mb-3">{right1()}</Box>
+//         <Box>{right2()}</Box>
+//       </div>
+//     </div>
+//   );
+// }
+
+// const form = appCategory('Forms');
+
+// form('Login', [
+//   ['username', () => <div>username</div>],
+//   ['username + password', () => <div>username + password</div>],
+//   ['forgot my password', () => <div>forgot my password</div>],
+//   ['passowrd reset', () => <div>passowrd reset</div>],
+// ]);
---
+++ stories/slide.md
@@ -0,0 +1,7 @@
+# Slide
+
+This component always has a ratio of 3:4
+
+## API
+
+**className** - this will be concatenated to the *Slide* component's className.
