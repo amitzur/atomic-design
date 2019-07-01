e479f75239fd9b515b7393a98a1a24540b9884e7
Amit Zur <sendwithchibo@gmail.com>
2019-07-01 17:09:15 +0300

    visual language for presentation

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -3,10 +3,25 @@ import React from 'react';
 import {addDecorator} from '@storybook/react';
 import {action} from '@storybook/addon-actions';
 
-import {designToken, atom} from '../.storybook/utils';
+// import {designToken, atom} from '../.storybook/utils'; // using this from another file kills HMR. Need to open an issue about this
+import {storiesOf} from '@storybook/react';
+
+const category = name => (kindName, stories) => {
+  const kind = storiesOf(`Library|${name}/${kindName}`, module);
+  for (const story of stories) {
+    kind.add(...story);
+  }
+};
+
+export const designToken = category('Design Tokens');
+export const atom = category('Atoms');
 
 addDecorator(story => (
-  <div className="stretch d-flex justify-content-center align-items-center">{story()}</div>
+  <div
+    className="stretch d-flex justify-content-center align-items-center"
+    style={{background: '#3f3f3f'}}>
+    <div className="box-shadow-lg bg-white p-5">{story()}</div>
+  </div>
 ));
 
 designToken('Colors', [
